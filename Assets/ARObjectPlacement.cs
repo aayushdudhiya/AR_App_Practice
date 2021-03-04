1e0fcbf3e1fab946de036ef66372dcb147bbd6a5
julia.ronneberger <julia@ronneberger.net>
2021-03-04 13:58:23 +0000

    first commit

---
+++ Assets/ARObjectPlacement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARObjectPlacement : MonoBehaviour
+{
+
+    public ARSessionOrigin origin;
+    public List<ARRaycastHit> raycasthits = new List<ARRaycastHit>();
+    public GameObject cube;
+    GameObject instantiatedCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Detect touch
+        // project raycast
+        // instantiate a virtual cube
+
+
+        if (Input.GetMouseButton(0))
+        {
+            bool collision = origin.GetComponent<ARRaycastManager>().Raycast(Input.mousePosition, raycasthits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon);
+            
+            if(collision)
+            {
+                if(instantiatedCube == null)
+                {
+                    instantiatedCube = Instantiate(cube);
+                }
+
+                instantiatedCube.transform.position = raycasthits[0].pose.position;
+            }
+        }
+
+       
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is just a practice project for using ARFoundation in Unity to create an AR app for Android
